d620d2a70922505c1e86e4a039b90195c6376546
ArbiaBougamra <arbia.bougamra@esprit.tn>
2020-11-23 00:51:35 +0100

    Splash Screen Progression.

--- app/src/main/java/com/esprit/miniproject/SplashActivity.java
+++ app/src/main/java/com/esprit/miniproject/SplashActivity.java
@@ -3,12 +3,29 @@ package com.esprit.miniproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView applogo, appname, appsplash;
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        applogo = findViewById(R.id.applogo);
+        appname = findViewById(R.id.appname);
+        appsplash = findViewById(R.id.appsplash);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        appsplash.animate().translationY(-3000).setDuration(1000).setStartDelay(4000);
+        applogo.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
+        appname.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+
     }
 }
