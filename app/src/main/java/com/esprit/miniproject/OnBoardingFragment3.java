066e839fb797ce0be70e3b080f20c5c6220e0b86
ArbiaBougamra <arbia.bougamra@esprit.tn>
2020-11-24 01:09:31 +0100

    Onboarding done.

--- app/src/main/java/com/esprit/miniproject/OnBoardingFragment3.java
+++ app/src/main/java/com/esprit/miniproject/OnBoardingFragment3.java
@@ -1,9 +1,11 @@
 package com.esprit.miniproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +17,14 @@ public class OnBoardingFragment3 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_on_boarding3, container, false);
+        TextView skipbtn = (TextView) root.findViewById(R.id.skipbtn);
+        skipbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
         return root;
     }
 }
